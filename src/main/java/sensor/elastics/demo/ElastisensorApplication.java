3c21fd81457c9c3f27f6e76b911c96987ace6a6f
filippomedri <filippo.medri@gmail.com>
2019-02-03 10:14:17 -0800

    First Commit

---
+++ src/main/java/sensor/elastics/demo/ElastisensorApplication.java
@@ -0,0 +1,14 @@
+package sensor.elastics.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElastisensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElastisensorApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/sensor/elastics/demo/ElastisensorApplicationTests.java
@@ -0,0 +1,17 @@
+package sensor.elastics.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElastisensorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
